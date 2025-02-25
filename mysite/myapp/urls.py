7ab6ffa7c8a5b018a401aaad2af523337ab1bfe3
Danil <danilfs009@yandex.ru>
2025-02-25 15:50:24 +0300

    base setup, views, models

---
+++ mysite/myapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from myapp.views import index,contacts
+
+
+urlpatterns = [
+  #Это url самого приложения app,если в первом параметре указать значение, например "hello", то оно будет всегда идти после главного "myapp", главные url находятся в mysite/mysite/urls
+
+  # http://127.0.0.1:8000/myapp/hello
+  path('hello', index),
+  # http://127.0.0.1:8000/myapp/contacts
+  path('contacts', contacts)
+]
---
+++ mysite/myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+
+def index(request):
+  items= Product.objects.all()
+  context = {
+    'items' : items,
+  }
+  return render(request, "myapp/index.html", context)
+
+def contacts(request):
+  return render(request, "myapp/contacts.html")
