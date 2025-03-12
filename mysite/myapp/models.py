00c7e82f7721f0ee9aca193c1b04dcff6d931b4d
Danil <danilfs009@yandex.ru>
2025-03-12 15:14:00 +0300

    update item

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Product(models.Model):
   name = models.CharField(max_length=100)
   price = models.IntegerField(blank=False, null=True)
-  description = models.CharField(max_length=400, blank=False, null=True)
+  description = models.CharField(max_length=400,null=True)
   image = models.ImageField(blank=True, upload_to="images")
 
   def __str__(self):
