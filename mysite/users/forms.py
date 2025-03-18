641586544a5f31cf6ae66456b5e77906210c3c33
Danil <danilfs009@yandex.ru>
2025-03-18 17:11:17 +0300

    finished user creation

--- mysite/users/forms.py
+++ mysite/users/forms.py
@@ -4,10 +4,17 @@ from django import forms
 
 class NewUserForm(UserCreationForm):
   email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'focus:outline-none', 'placeholder': 'mail@mail.com'}))
-  username = forms.CharField(required=True, widget=forms.EmailInput(attrs={'class': 'focus:outline-none', 'placeholder': 'Enter username...'}))
-  password1 = forms.CharField(required=True, widget=forms.EmailInput(attrs={'class': 'focus:outline-none', 'placeholder': 'Enter password'}))
-  password2 = forms.CharField(required=True, widget=forms.EmailInput(attrs={'class': 'focus:outline-none', 'placeholder': 'Enter same password'}))
+  username = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'focus:outline-none', 'placeholder': 'Enter username...'}))
+  password1 = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'class': 'focus:outline-none', 'placeholder': 'Enter password'}))
+  password2 = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'class': 'focus:outline-none', 'placeholder': 'Enter same password'}))
 
   class Meta:
     model = User
     fields = ('username', 'email', 'password1', 'password2')
+
+  # def save(self, commit=True):
+  #   user = super(NewUserForm,self).save(commit=False)
+  #   user.email = self.cleaned_date['email']
+  #   if commit:
+  #     user.save()
+  #   return user
--- mysite/users/urls.py
+++ mysite/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import register
+from django.contrib.auth.views import LoginView
 
 app_name = "users"
 
@@ -8,6 +9,7 @@ urlpatterns = [
 
   # http://127.0.0.1:8000/myapp/hello
   path('register/', register, name='register'),
+  path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
   
   
 ]
